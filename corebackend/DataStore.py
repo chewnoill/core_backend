d8c49551175327208abce81875fe70aedaf24fd7
chew no ill <chew.no.ill@gmail.com>
2013-03-26 07:10:44 -0400

    data store

---
+++ corebackend/DataStore.py
@@ -0,0 +1,55 @@
+'''
+Created on Mar 23, 2013
+
+@author: will
+'''
+from google.appengine.ext import db
+
+class Menu(db.Model):
+    name = db.StringProperty(required=True)
+    JSONmenu = db.TextProperty()
+    
+def insertMenu(name,JSON):
+    #check if exists already
+    found = db.GqlQuery("select * from Menu where name='%s'" % name)
+    db.delete(found)
+    m = Menu(name=name)
+    if(JSON):
+        m.JSONmenu=JSON
+    m.put()
+    
+
+def getMenu(name):
+    found = db.GqlQuery("select * from Menu where name='%s'" % name)
+    ret = found.get()
+    if not ret==None:
+        return ret.JSONmenu
+    return ret
+
+class PathLookup(db.Model):
+    arg1 = db.StringProperty(required=True)
+    arg2 = db.StringProperty()
+    path = db.StringProperty(required=True)
+    
+def insertPath(arg1,arg2,path):
+    found = None
+    if arg2:
+        found = db.GqlQuery("select * from PathLookup where arg1='%s' and arg2='%s'" % (arg1,arg2))
+    else:
+        found = db.GqlQuery("select * from PathLookup where arg1='%s'" % arg1)
+    
+    for p in found.run(limit=50):
+        p.delete()
+    m = PathLookup(arg1=arg1,
+                   path=path)
+    if arg2:
+        m.arg2=arg2
+    m.put()
+def getPath(arg1,arg2=None):
+    found = None
+    if arg2:
+        found = db.GqlQuery("select * from PathLookup where arg1='%s' and arg2='%s'" % (arg1,arg2))
+    else:
+        found = db.GqlQuery("select * from PathLookup where arg1='%s'" % arg1)
+    if found and found.get():
+        return found.get().path
